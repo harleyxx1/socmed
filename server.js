ea96a369d56906557cdbe4a6edba7758c145532e
Ding Harley Catubag <dingharley123@gmail.com>
2021-01-31 04:21:58 +0800

    #commit socket chagnes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cloudinaryConnect = require('./backend/config/cloudinary');
 const userRoutes = require('./backend/routes/userRoutes');
 const postRoutes = require('./backend/routes/postRoutes');
 const commentRoutes = require('./backend/routes/commentRoutes');
-const Socket = require('./backend/classes/SocketIO');
 
 const { errorHandler } = require('./backend/middleware/errorMiddlewares');
 
@@ -27,17 +26,6 @@ cloudinaryConnect();
  */
 const app = express();
 
-/**
- * Socket.io initialization.
- */
-const socketIO = new Socket(app);
-const http = socketIO.getHttp();
-
-/**
- * Socket.io connecting the user.
- */
-socketIO.connect()
-
 app.use(express.json());
 
 app.use(express.static('uploads'));
@@ -53,4 +41,4 @@ app.use('/api/comments', commentRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-http.listen(PORT, console.log(`App is runing at port ${PORT}`));
+app.listen(PORT, console.log(`App is runing at port ${PORT}`));
